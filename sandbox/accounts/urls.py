cc682fff3cda6ffccf6aa702a28ec7c13a366171
hibiki-iwamoto <hibiki.iwamoto.ict@gmail.com>
2025-05-04 05:53:52 +0000

    Fix

--- sandbox/accounts/urls.py
+++ sandbox/accounts/urls.py
@@ -6,7 +6,5 @@ urlpatterns = [
         template_name='accounts/login.html', 
         redirect_authenticated_user=True
         ), name='login'),
-    path('logout/', LogoutView.as_view(
-        template_name='accounts/logout.html'), 
-         name='logout'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
